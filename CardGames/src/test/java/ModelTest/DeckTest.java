fc7d8618e4325607420524d2956fb77d788bb35a
j.c. <jcbackenddev@gmail.com>
2020-07-23 13:38:24 +0200

    Unit Test created
    I created some unit test in order to see where the NullPointerException
    comes from. The debugger did not show any more useful information. In
    order to implement JUnit test, I had to change the clathpath.

---
+++ CardGames/src/test/java/ModelTest/DeckTest.java
@@ -0,0 +1,40 @@
+package ModelTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.Model.Deck;
+
+class DeckTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		// ARRANGE
+		Deck deck = new Deck();
+		
+		// ACT & ASSERT
+		assertEquals(deck.numberOfCardsinDeck(), 52);
+	}
+
+}
